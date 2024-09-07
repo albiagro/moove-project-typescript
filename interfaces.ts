6ced052cc6375ef05cde75bf2079dd50979cb255
Alberto Agrò <134003520+albiagro@users.noreply.github.com>
2024-09-07 18:35:59 +0200

    Update interfaces.ts

--- interfaces.ts
+++ interfaces.ts
@@ -1,20 +1,20 @@
-export interface IMezzo {
-    tipo: "bici" | "scooter" | "monopattino"
-    id: number
-    stato: "disponibile" | "in uso"
-    assegnaUtente(utenteDaAssegnare : IUtente) : boolean
- }
- 
- export interface IUtente {
-    nome: string
-    cognome: string
-    email: string
-    metodoPagamento: "paypal" | "mooney" | "carta"
-    prenotaMezzo(mezzoDaAssegnare : IMezzo) : void
- }
- 
- export interface ICitta {
-    nome: string
-    mezziDisponibili: IMezzo[]
-    aggiungiMezzo(mezzoDaAggiungere : IMezzo) : void
- }
+export interface IVehicle {
+    type: "bike" | "scooter" | "electric scooter";
+    id: number;
+    status: "available" | "in use";
+    assignUser(userToAssign: IUser): boolean;
+}
+
+export interface IUser {
+    name: string;
+    surname: string;
+    email: string;
+    paymentMethod: "paypal" | "mooney" | "card";
+    bookVehicle(vehicleToAssign: IVehicle): void;
+}
+
+export interface ICity {
+    name: string;
+    availableVehicles: IVehicle[];
+    addVehicle(vehicleToAdd: IVehicle): void;
+}
